adfd4f7b307d4f1d86d2d5ef32141db6f6c100ce
Hemalatha192472042 <maramhemalatha8@gmail.com>
2026-07-31 16:47:56 +0530

    Update EXP2.py

--- EXP2.py
+++ EXP2.py
@@ -1,22 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 sales_data = np.genfromtxt(
-    "../Datasets/sales_data.csv",
+    "sales_data.csv",
     delimiter=",",
     skip_header=1
 )
-
 average_price = np.mean(sales_data)
-
-print("Average Price of All Products =", round(average_price,2))
+print("Average Price of All Products =", round(average_price, 2))
 product_average = np.mean(sales_data, axis=0)
 products = ["Product1", "Product2", "Product3"]
-
+plt.figure(figsize=(6, 4))
 plt.bar(products, product_average)
-
 plt.title("Average Sales of Products")
 plt.xlabel("Products")
 plt.ylabel("Average Price")
-
-plt.show()
+plt.savefig("sales_graph.png")
+print("Graph saved as sales_graph.png")
