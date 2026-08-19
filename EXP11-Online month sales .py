d5a595b5275b096b2ec65482247630ea3d626a68
Hemalatha192472042 <maramhemalatha8@gmail.com>
2026-08-19 11:44:30 +0530

    Update and rename EXP11.py to EXP11-Online month sales .py

--- EXP11-Online month sales .py
+++ EXP11-Online month sales .py
@@ -1,3 +1,14 @@
+#11. Scenario : You are a data scientist working for a company that sells products online. You have been tasked with creating a simple plot to show the sales 
+#of a product over time.
+#Question: 
+#1. Write code to create a simple line plot in Python using Matplotlib to predict sales happened in a month? 
+#2. Write code to create a scatter plot in Python using Matplotlib to predict sales happened in a month? 
+#3. Develop a Python program to create a bar plot of the monthly sales data. 
+
+
+
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
